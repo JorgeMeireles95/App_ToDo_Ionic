52b07d46faa9e1bd279a70b9d16cb1b9e78f6f75
Jorge <jorgeantoniomeireles@gmail.com>
2021-06-13 18:34:47 -0300

    Salvando no Local Storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +8,16 @@ import { AlertController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(private alertCtrl: AlertController ) {}
+  tasks : any[] = [];
+
+  constructor(private alertCtrl: AlertController, private toastCtrl: ToastController ) {}
 
    async showAdd(){
     const alert = await this.alertCtrl.create({
       header: 'O que deseja fazer?',
       inputs: [
         {
-          name:'task',
+          name:'newTask',
           type:'text',
           placeholder:'O que desejar fazer?'
         }
@@ -33,12 +35,42 @@ export class HomePage {
         {
           text:'Adicionar',
           handler: (form)=>{
-           // this.add(form.taskTodo);
+          // console.log(form.newTask);
+           
+            this.add(form.newTask);
           }
         }
       ]
     });
     await alert.present();
+  }
+
+
+
+  async add(newTask : string){
+    // VALIDA SE O USUARIO PREENCHEU A TASK=TAREFA
+    if(newTask.trim().length < 1){
+      const toast = await this.toastCtrl.create({
+        message : 'Informe o que deseja fazer!',
+        duration: 2000,
+        position : 'top',
+      // Vericar como mudar a cor do toast // cssClass: 'toast-scheme',
+      });
+
+      toast.present();
+      return;
+    }
+
+    let task = {name : newTask, done: false};
+
+    this.tasks.push(task);
+
+    this.updateLocalStorage();
+  }
+
+
+  updateLocalStorage(){
+    localStorage.setItem('taskDB',JSON.stringify(this.tasks));
   }
 
 }
